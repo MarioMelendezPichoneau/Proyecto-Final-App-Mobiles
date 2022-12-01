3a6bd91672e830ad3fcf99150f7ad584b3625d33
MarioMelendezPichoneau <20197959@itla.edu.do>
2022-12-01 11:56:05 -0400

    cors reaprado

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { HttpClient, HttpHeaderResponse, HttpHeaders}  from '@angular/common/http';
+
+
 
 @Component({
   selector: 'app-home',
@@ -7,6 +10,43 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  private urlApi = "def/noticias.php";
+  //apiUrl: string = "https://adamix.net/defensa_civil/def/noticias.php";
+  headers = new HttpHeaders().set('Content-Type', 'application/json');
+
+
+
+
+  constructor(private http: HttpClient) {}
+
+  Noticias: any = [];
+
+  ngOnInit(): void {
+
+    this.getnoticas();
+
+  }
+
+
+  getnoticas(){
+    this.http.get(this.urlApi).subscribe((res)=>{
+      this.Noticias = res;
+
+    }, (error=>{
+      console.error(error);
+    }));
+  }
+
+
+
+}
+
+export interface Noticias{
+  id: number;
+  fecha: string;
+  titulo: string;
+  contenido: string;
+  foto: string;
+
 
 }
